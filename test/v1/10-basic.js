b4d37cae603a1372e87d8bd9ca221b79097c28f2
andrewjones <ajones@digitalbazaar.com>
2019-06-06 15:46:37 -0400

    Start tests.

---
+++ test/v1/10-basic.js
@@ -0,0 +1,11 @@
+describe.skip('DiD Test Suite', function() {
+  it('A DID method specification MUST further restrict the generic DID syntax by defining its own method-name and its own method-specific-id syntax.', function() {
+
+  });
+  it('A method-specific parameter name MUST be prefixed by the method name as defined by the method-name rule.', function() {
+
+  });
+  it('', function() {
+
+  })
+})
