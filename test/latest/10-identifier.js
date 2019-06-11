1545e25bd97931e791abca91351b72e144154dc7
andrewjones <ajones@digitalbazaar.com>
2019-06-11 11:49:32 -0400

    Add tests for Identifiers.

--- test/latest/10-identifier.js
+++ test/latest/10-identifier.js
@@ -18,16 +18,17 @@ describe('Identifier', function() {
   it('The did: scheme name MUST be lowercase.', async function() {
     let error = null;
     try {
-      await util.generate('uppercase-did.txt', generatorOptions);
+      await util.generate('uppercase.did', generatorOptions);
     } catch(e) {
       error = e;
     }
     expect(error, 'Expected an Error to be Thrown').to.not.be.null;
   });
+
   it('(did) The method name MUST be lowercase.', async function() {
     let error = null;
     try {
-      await util.generate('uppercase-method-name.txt', generatorOptions);
+      await util.generate('uppercase-method-name.did', generatorOptions);
     } catch(e) {
       error = e;
     }
@@ -35,27 +36,31 @@ describe('Identifier', function() {
   });
 
   it('A DID method specification MUST further restrict the generic DID syntax by defining its own method-name and its own method-specific-id syntax.', async function() {
-    await util.generate('valid-did.txt', generatorOptions);
-    expect('did:uuid:0d2bae3e-4915-489c-bfa1-1ba14e8b43cd').to.be.a.DID;
+    await util.generate('valid.did', generatorOptions);
   });
+
   it('A method-specific parameter name MUST be prefixed by the method name as defined by the method-name rule.', async function() {
-    throw new Error('Not Implemented');
+    await util.generate('method-specific-param.did', generatorOptions);
 
   });
+
+  it('optional Method-specific parameter names MAY be combined with generic parameter names in any order.', async function() {
+    await util.generate('method-specific-generic-params.did', generatorOptions);
+  });
+
   it('A generic DID path is identical to a URI path and MUST conform to the the path-abempty ABNF rule in [RFC3986].', async function() {
     // begins with "/" or is empty
     // basically this test means "/" or " " is a valid path
-    throw new Error('Not Implemented');
-
+    await util.generate('path.did', generatorOptions);
   });
+
   it('A generic DID query is identical to a URI query and MUST conform to the the query ABNF rule in [RFC3986].', async function() {
     // https://nodejs.org/api/querystring.html
     // https://stackoverflow.com/questions/23959352/validate-url-query-string-with-regex
-    throw new Error('Not Implemented');
-
+    await util.generate('query.did', generatorOptions);
   });
-  it('A generic DID fragment is identical to a URI fragment and MUST conform to the the fragment ABNF rule in [RFC3986]', async function() {
-    throw new Error('Not Implemented');
 
+  it('A generic DID fragment is identical to a URI fragment and MUST conform to the the fragment ABNF rule in [RFC3986]', async function() {
+    await util.generate('fragment.did', generatorOptions);
   });
 });
