9818f95c95394ccaf670b9e1c3383eb942993113
andrewjones <ajones@digitalbazaar.com>
2019-06-13 13:40:40 -0400

    Organize inputs for tests.

--- test/latest/10-identifier.js
+++ test/latest/10-identifier.js
@@ -16,41 +16,41 @@ describe('Identifier', function() {
   });
   describe(' positive ', function() {
     it('The did: scheme name MUST be lowercase.', async function() {
-      await util.generate('valid.did', generatorOptions);
+      await util.generate('did/valid.did', generatorOptions);
     });
 
     it('(did) The method name MUST be lowercase.', async function() {
-      await util.generate('valid.did', generatorOptions);
+      await util.generate('did/valid.did', generatorOptions);
     });
 
     it('A DID method specification MUST further restrict the generic DID syntax by defining its own method-name and its own method-specific-id syntax.', async function() {
-      await util.generate('valid.did', generatorOptions);
+      await util.generate('did/valid.did', generatorOptions);
     });
 
     it('A method-specific parameter name MUST be prefixed by the method name as defined by the method-name rule.', async function() {
-      await util.generate('method-specific-param.did', generatorOptions);
+      await util.generate('did/method-specific-param.did', generatorOptions);
     });
     it('A generic DID path is identical to a URI path and MUST conform to the the path-abempty ABNF rule in [RFC3986].', async function() {
       // begins with "/" or is empty
       // basically this test means "/" or " " is a valid path
-      await util.generate('path.did', generatorOptions);
+      await util.generate('did/path.did', generatorOptions);
     });
 
     it('A generic DID query is identical to a URI query and MUST conform to the the query ABNF rule in [RFC3986].', async function() {
       // https://nodejs.org/api/querystring.html
       // https://stackoverflow.com/questions/23959352/validate-url-query-string-with-regex
-      await util.generate('query.did', generatorOptions);
+      await util.generate('did/query.did', generatorOptions);
     });
 
     it('A generic DID fragment is identical to a URI fragment and MUST conform to the the fragment ABNF rule in [RFC3986]', async function() {
-      await util.generate('fragment.did', generatorOptions);
+      await util.generate('did/fragment.did', generatorOptions);
     });
   });
   describe(' negative ', function() {
     it('The did: scheme name MUST be lowercase.', async function() {
       let error = null;
       try {
-        await util.generate('uppercase.did', generatorOptions);
+        await util.generate('did/uppercase.did', generatorOptions);
       } catch(e) {
         error = e;
       }
@@ -60,7 +60,7 @@ describe('Identifier', function() {
     it('(did) The method name MUST be lowercase.', async function() {
       let error = null;
       try {
-        await util.generate('uppercase-method-name.did', generatorOptions);
+        await util.generate('did/uppercase-method-name.did', generatorOptions);
       } catch(e) {
         error = e;
       }
@@ -70,7 +70,7 @@ describe('Identifier', function() {
     it('A DID method specification MUST further restrict the generic DID syntax by defining its own method-name and its own method-specific-id syntax.', async function() {
       let error = null;
       try {
-        await util.generate('no-method-name.did', generatorOptions);
+        await util.generate('did/no-method-name.did', generatorOptions);
       } catch(e) {
         error = e;
       }
@@ -80,7 +80,7 @@ describe('Identifier', function() {
     it('A method-specific parameter name MUST be prefixed by the method name as defined by the method-name rule.', async function() {
       let error = null;
       try {
-        await util.generate('invalid-method-parameter.did', generatorOptions);
+        await util.generate('did/invalid-method-parameter.did', generatorOptions);
       } catch(e) {
         error = e;
       }
@@ -89,7 +89,7 @@ describe('Identifier', function() {
   });
   describe('optional ', function() {
     it('Method-specific parameter names MAY be combined with generic parameter names in any order.', async function() {
-      await util.generate('method-specific-generic-params.did', generatorOptions);
+      await util.generate('did/method-specific-generic-params.did', generatorOptions);
     });
   });
 
